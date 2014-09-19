4d225f9e39689a8c91f7935dccfc6bd0ef42663c
HIT Schueler <hit.schueler@gmx.at>
2014-09-19 07:14:17 +0200

    Py01: with sphinx

--- bruch/doc/source/conf.py
+++ bruch/doc/source/conf.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# fraction documentation build configuration file, created by
-# sphinx-quickstart on Wed Sep 17 22:03:17 2014.
+# Bruch documentation build configuration file, created by
+# sphinx-quickstart on Fri Sep 19 06:47:14 2014.
 #
 # This file is execfile()d with the current directory set to its
 # containing dir.
@@ -46,7 +46,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'fraction'
+project = 'Bruch'
 copyright = '2014, Walter Rafeiner-Magor'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -70,7 +70,7 @@ release = '1.1'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = []
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
@@ -179,7 +179,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'fractiondoc'
+htmlhelp_basename = 'Bruchdoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -199,7 +199,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'fraction.tex', 'fraction Documentation',
+  ('index', 'Bruch.tex', 'Bruch Documentation',
    'Walter Rafeiner-Magor', 'manual'),
 ]
 
@@ -229,7 +229,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'fraction', 'fraction Documentation',
+    ('index', 'bruch', 'Bruch Documentation',
      ['Walter Rafeiner-Magor'], 1)
 ]
 
@@ -243,8 +243,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'fraction', 'fraction Documentation',
-   'Walter Rafeiner-Magor', 'fraction', 'One line description of project.',
+  ('index', 'Bruch', 'Bruch Documentation',
+   'Walter Rafeiner-Magor', 'Bruch', 'One line description of project.',
    'Miscellaneous'),
 ]
 
---
+++ oop/doc/source/oop.rst
@@ -0,0 +1,9 @@
+oop.employee1 module
+--------------------
+
+.. py:currentmodule:: oop.employee1
+.. autoclass:: Employee
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
