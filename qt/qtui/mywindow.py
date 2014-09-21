a72593d90e3d97b9e195b0a199e9632fa0f74d9e
Hit Schueler <hit.schueler@gmx.at>
2014-09-21 10:10:15 +0200

    qt added

---
+++ qt/qtui/mywindow.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+__author__ = 'uhs374h'
+import sys
+from PyQt4 import QtGui, uic
+
+class MyWindow(QtGui.QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        uic.loadUi('mywindow.ui', self)
+        self.show()
+
+if __name__ == '__main__':
+    app = QtGui.QApplication(sys.argv)
+    window = MyWindow()
+    sys.exit(app.exec_())
