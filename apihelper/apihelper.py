532503f9642cd4001728f1a2c550b706c6cad418
peterfuchs1 <peterfuchs_1@yahoo.de>
2014-11-23 15:55:33 +0100

    apihelper added documentation added

---
+++ apihelper/apihelper.py
@@ -0,0 +1,63 @@
+"""Cheap and simple API helper
+
+This program is part of "Dive Into Python", a free Python book for
+experienced programmers.  Visit http://diveintopython.org/ for the
+latest version.
+with enhancements for attributes (c) walter rafeiner-Magor
+"""
+
+__author__ = "Mark Pilgrim (mark@diveintopython.org)"
+__version__ = "$Revision: 2.0 $"
+__date__ = "$Date: 2014/11/23 21:57:19 $"
+__copyright__ = "Copyright (c) 2001 Mark Pilgrim"
+__license__ = "Python"
+
+# While this is a good example script to teach about introspection,
+# in real life it has been superceded by PyDoc, which is part of the
+# standard library in Python 2.1 and later.
+#
+# Your IDE may already import the "help" function from pydoc
+# automatically on startup; if not, do this:
+#
+# >>> from pydoc import help
+#
+# The help function in this module takes the object itself to get
+# help on, but PyDoc can also take a string, like this:
+#
+# >>> help("string") # gets help on the string module
+# >>> help("apihelper.help") # gets help on the function below
+# >>> help() # enters an interactive help mode
+#
+# PyDoc can also act as an HTTP server to dynamically produce
+# HTML-formatted documentation of any module in your path.
+# That's wicked cool.  Read more about PyDoc here:
+#   http://www.onlamp.com/pub/a/python/2001/04/18/pydoc.html
+
+def info(object, spacing=10, collapse=1, **kwargs):
+	"""Print methods with doc strings and attributes.
+
+	:param object:	the object to introspect (modules, classes, lists, dictionaries and strings
+	:param spacing:	default 10; how many spaces between elements and the description
+	:param collapse:	default 1; shall we consume all white spaces ?
+	:param attributes:	default False; shall we show all attributes too?
+	"""
+	#let's iterate thru the keyword arguments
+	attributes=False
+	for k,v in kwargs.items():
+		if k=='attributes':
+			if v: attributes=True
+
+	methodList = [e for e in dir(object) if callable(getattr(object, e))]
+	attributList= [e for e in dir(object) if not callable(getattr(object, e))]
+	processFunc = collapse and (lambda s: " ".join(s.split())) or (lambda s: s)
+	print("\n"+"=".ljust(spacing,'=')+"\nMethods:")
+	print ("\n".join(["%s %s" %
+					 (method.ljust(spacing),
+					  processFunc(str(getattr(object, method).__doc__)))
+					 for method in methodList]))
+	if attributes:
+		print("\n"+"=".ljust(spacing,'=')+"\nAttributes:")
+		print ("\n".join([attribut for attribut in attributList]))
+if __name__ == "__main__":
+	print (help.__doc__)
+
